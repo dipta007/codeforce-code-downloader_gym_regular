87f017df085de9e9f3da7313871e6a5c3392b243
shubhashis <Shubhasish@ipvisionsoft.com>
2017-11-25 14:31:37 +0600

    #2 added downloaded file to keep track of the dowloaded submissions

--- codeforces-downloader.py
+++ codeforces-downloader.py
@@ -27,184 +27,211 @@ keys = replacer.keys()
 waitTime = 4
 
 gym = {
-    
+	
 }
 
 regular = {
-    
+	
 }
 
 def GetPathOfChromeDriver():
-    path = os.getcwd() + "/chromedriver"
-    if _platform == "linux" or _platform == "linux2":
-       # linux
-       path += "_linux"
-    elif _platform == "darwin":
-       # MAC OS X
-       path += "_mac"
-    elif _platform == "win32" or _platform == "win64":
-       # Windows
-       path += "_win"
-    return path
+	path = os.getcwd() + "/chromedriver"
+	if _platform == "linux" or _platform == "linux2":
+	   # linux
+	   path += "_linux"
+	elif _platform == "darwin":
+	   # MAC OS X
+	   path += "_mac"
+	elif _platform == "win32" or _platform == "win64":
+	   # Windows
+	   path += "_win"
+	return path
 
 driver = webdriver.Chrome( GetPathOfChromeDriver() )
 
 
 def GetContestName():
-    site = "http://codeforces.com/api/contest.list?gym=true"
-    contestInfo = urllib.urlopen(site).read()
-    dic = json.loads(contestInfo)
-    if dic["status"] != "OK":
-        print "Oops.. Something went wrong...."
-        driver.quit()
-        exit(0)
+	site = "http://codeforces.com/api/contest.list?gym=true"
+	contestInfo = urllib.urlopen(site).read()
+	dic = json.loads(contestInfo)
+	if dic["status"] != "OK":
+		print "Oops.. Something went wrong...."
+		driver.quit()
+		exit(0)
 
-    contests = dic['result']
-    for contest in contests:
-        contestID = contest['id']
-        contestName = contest['name']
-        gym[contestID] = contestName
+	contests = dic['result']
+	for contest in contests:
+		contestID = contest['id']
+		contestName = contest['name']
+		gym[contestID] = contestName
 
-    site = "http://codeforces.com/api/contest.list?gym=false"
-    contestInfo = urllib.urlopen(site).read()
-    dic = json.loads(contestInfo)
-    if dic["status"] != "OK":
-        print "Oops.. Something went wrong...."
-        driver.quit()
-        exit(0)
+	site = "http://codeforces.com/api/contest.list?gym=false"
+	contestInfo = urllib.urlopen(site).read()
+	dic = json.loads(contestInfo)
+	if dic["status"] != "OK":
+		print "Oops.. Something went wrong...."
+		driver.quit()
+		exit(0)
 
-    contests = dic['result']
-    for contest in contests:
-        contestID = contest['id']
-        contestName = contest['name']
-        regular[contestID] = contestName
+	contests = dic['result']
+	for contest in contests:
+		contestID = contest['id']
+		contestName = contest['name']
+		regular[contestID] = contestName
 
 def get_ext(comp_lang):
-    if 'C++' in comp_lang:
-        return 'cpp'
-    for key in EXT_keys:
-        if key in comp_lang:
-            return EXT[key]
-    return ""
+	if 'C++' in comp_lang:
+		return 'cpp'
+	for key in EXT_keys:
+		if key in comp_lang:
+			return EXT[key]
+	return ""
 
 def parse(source_code):
-    for key in keys:
-        source_code = source_code.replace(key, replacer[key])
-    return source_code
+	for key in keys:
+		source_code = source_code.replace(key, replacer[key])
+	return source_code
 
 def CFLogIn(user, passwd):
-    login_site = "http://codeforces.com/enter"
-    driver.get(login_site)
-    time.sleep(waitTime)
-    username = driver.find_element_by_id("handle")
-    password = driver.find_element_by_id("password")
-    
-    username.send_keys(user)
-    password.send_keys(passwd)
+	login_site = "http://codeforces.com/enter"
+	driver.get(login_site)
+	username = driver.find_element_by_id("handle")
+	password = driver.find_element_by_id("password")
+	
+	username.send_keys(user)
+	password.send_keys(passwd)
 
-    driver.find_element_by_class_name("submit").click()
-    time.sleep(waitTime)
-    elem = driver.find_elements_by_css_selector("#enterForm > table > tbody > tr.subscription-row > td:nth-child(2) > div > span")
-    if len(elem) > 0:
-        print "Invalid Handle / Password"
-        driver.quit()
-        exit(0)
-    time.sleep(waitTime)
+	driver.find_element_by_class_name("submit").click()
+
+	elem = driver.find_elements_by_css_selector("#enterForm > table > tbody > tr.subscription-row > td:nth-child(2) > div > span")
+	if len(elem) > 0:
+		print "Invalid Handle / Password"
+		driver.quit()
+		exit(0)
+	time.sleep(waitTime)
 
 def FileNameParse(file):
-    avoid = ['<', '>', ':','"', '/', '\\', '|', '?', '*']
-    ret = ""
-    for ch in file:
-        if ch not in avoid:
-            ret += ch
-    return ret
+	avoid = ['<', '>', ':','"', '/', '\\', '|', '?', '*']
+	ret = ""
+	for ch in file:
+		if ch not in avoid:
+			ret += ch
+	return ret
+
+def GetDownloadedFile(handle):
+	path = handle + '/' + 'downloaded'
+	if os.path.exists(path) == False:
+		return []
+	file = open(str(path), 'r')
+	downloaded = file.readlines()
+	downloaded = map(lambda s: s.strip(), downloaded)
+	print "Existing: ", downloaded
+	return downloaded
+
+def SetDownloadedFile(handle, downloaded):
+	path = handle + '/' + 'downloaded'
+	file = open(str(path), 'w')
+	print "New: ", downloaded
+	file.write( "\n".join(downloaded))
 
 def main():
-    handle = raw_input("Enter your handle: ")
-    print ("Next step is password. ;) ")
-    print ( "If you are afraid then check the code, You are smart enough to understand it")
-    passwd = getpass.getpass("Enter your password: ")
+	handle = raw_input("Enter your handle: ")
+	print ("Next step is password. ;) ")
+	print ( "If you are afraid then check the code, You are smart enough to understand it")
+	passwd = getpass.getpass("Enter your password: ")
 
-    CFLogIn(handle, passwd)
-    GetContestName()
+	start_time = time.time()
 
-    if not os.path.exists(handle):
-        os.makedirs(handle)
+	CFLogIn(handle, passwd)
+	GetContestName()
+	downloaded = GetDownloadedFile(handle)
 
-    user_info = urllib.urlopen(USER_INFO_URL.format(handle=handle, count=MAX_SUBS)).read()
-    dic = json.loads(user_info)
+	if not os.path.exists(handle):
+		os.makedirs(handle)
 
-    if dic['status'] != u'OK':
-        print ('Oops.. Something went wrong...')
-        exit(0)
+	user_info = urllib.urlopen(USER_INFO_URL.format(handle=handle, count=MAX_SUBS)).read()
+	dic = json.loads(user_info)
 
-    submissions = dic['result']
-    start_time = time.time()
+	if dic['status'] != 'OK':
+		print ('Oops.. Something went wrong...')
+		exit(0)
 
-    for submission in submissions:
-        if submission['verdict'] == 'OK' and submission['contestId'] < MAX_CF_CONTEST_ID:
-            con_id, sub_id = submission['contestId'], submission['id'],
-            prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
-            comp_lang = submission['programmingLanguage']
-            
-            driver.get(SUBMISSION_URL.format(ContestId=con_id, SubmissionId=sub_id))
-            submission_info = driver.find_element_by_xpath("//*[@id=\"pageContent\"]/div[3]/pre").text
-            result = parse(submission_info).replace('\r', '')
-            ext = get_ext(comp_lang)
+	submissions = dic['result']
+	for submission in submissions:
+		if submission['verdict'] == 'OK' and submission['contestId'] < MAX_CF_CONTEST_ID:
+			con_id, sub_id = submission['contestId'], submission['id'],
+			prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
+			comp_lang = submission['programmingLanguage']
 
-            con_name = regular[con_id]
-            
-            new_directory = handle + '/' + FileNameParse(con_name + " - " + str(con_id))
-            path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext;
+			prob =  str(con_id) + str(prob_id)
+			if prob in downloaded:
+				continue
+			
+			driver.get(SUBMISSION_URL.format(ContestId=con_id, SubmissionId=sub_id))
+			submission_info = driver.find_element_by_xpath("//*[@id=\"pageContent\"]/div[3]/pre").text
+			result = parse(submission_info).replace('\r', '')
+			ext = get_ext(comp_lang)
 
-            if(len(new_directory) > 100):
-            	new_directory = handle + '/' + FileNameParse(str(con_id))
-            	path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext 
+			con_name = regular[con_id]
+			
+			new_directory = handle + '/' + FileNameParse(con_name + " - " + str(con_id))
+			path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext;
 
-            if not os.path.exists(new_directory):
-                os.makedirs(new_directory)
+			if(len(new_directory) > 100):
+				new_directory = handle + '/' + FileNameParse(str(con_id))
+				path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext 
 
-            
-            file = open(path, 'w')
+			if not os.path.exists(new_directory):
+				os.makedirs(new_directory)
 
-            file.write(result.encode('UTF-8'))
-            file.close()
-            print "Regular - ", str(prob_name.encode('UTF-8')) 
-            time.sleep(waitTime)
+			
+			file = open(path, 'w')
 
-        elif submission['verdict'] == 'OK':
-            con_id, sub_id = submission['contestId'], submission['id'],
-            prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
-            comp_lang = submission['programmingLanguage']
+			file.write(result.encode('UTF-8'))
+			file.close()
+			print "Regular - ", str(prob_name.encode('UTF-8'))
+			downloaded.append(str(con_id) + str(prob_id))
+			time.sleep(waitTime)
 
-            driver.get(SUBMISSION_URL_GYM.format(ContestId=con_id, SubmissionId=sub_id))
-            submission_info = driver.find_element_by_xpath("//*[@id=\"pageContent\"]/div[3]/pre").text
-            result = parse(submission_info).replace('\r', '')
-            ext = get_ext(comp_lang)
-            con_name = gym[con_id]
-            
-            new_directory = handle + '/' + FileNameParse(con_name + " - " + str(con_id))
-            path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name)  + '.' + ext
+		elif submission['verdict'] == 'OK':
+			con_id, sub_id = submission['contestId'], submission['id'],
+			prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
+			comp_lang = submission['programmingLanguage']
 
-            if(len(new_directory) > 100):
-            	new_directory = handle + '/' + FileNameParse(str(con_id))
-            	path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext 
+			prob = str(con_id) + str(prob_id)
+			if prob in downloaded:
+				continue
 
-            if not os.path.exists(new_directory):
-                os.makedirs(new_directory)
+			driver.get(SUBMISSION_URL_GYM.format(ContestId=con_id, SubmissionId=sub_id))
+			submission_info = driver.find_element_by_xpath("//*[@id=\"pageContent\"]/div[3]/pre").text
+			result = parse(submission_info).replace('\r', '')
+			ext = get_ext(comp_lang)
+			con_name = gym[con_id]
+			
+			new_directory = handle + '/' + FileNameParse(con_name + " - " + str(con_id))
+			path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name)  + '.' + ext
 
-            
-            file = open(path, 'w')
+			if(len(new_directory) > 100):
+				new_directory = handle + '/' + FileNameParse(str(con_id))
+				path = new_directory + '/' + FileNameParse(str(con_id) + str(prob_id) + "-" + prob_name) + '.' + ext 
 
-            file.write(result.encode('UTF-8'))
-            file.close()
-            print "Gym - ", str(prob_name.encode('UTF-8')) 
-            time.sleep(waitTime)
+			if not os.path.exists(new_directory):
+				os.makedirs(new_directory)
 
-    end_time = time.time()
-    driver.quit()
+			
+			file = open(path, 'w')
 
-    print ('Execution time %d seconds' % int(end_time - start_time) )
+			file.write(result.encode('UTF-8'))
+			file.close()
+			print "Gym - ", str(prob_name.encode('UTF-8'))
+			downloaded.append(str(con_id) + str(prob_id))
+			time.sleep(waitTime)
+
+	end_time = time.time()
+	driver.quit()
+	SetDownloadedFile(handle, downloaded)
+
+	print ('Execution time %d seconds' % int(end_time - start_time) )
 
 if __name__ == "__main__":
-    main()
+	main()
